5cf7de5ada1fb93265d72deb925d18e8f5d66e26
Osvaldo <osvaldo.ramirez@upr.edu>
2017-12-03 18:27:27 -0400

    Completing the post request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=JUMPTYSON1234';
 
 export function fetchPosts(){
@@ -10,6 +11,17 @@ export function fetchPosts(){
 
   return{
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback){
+  //console.log(values);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+  //console.log(request);
+  return{
+    type: CREATE_POST,
     payload: request
   };
 }
